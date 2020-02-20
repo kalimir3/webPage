424730d76ef263e0000dba0d5fcccdb0e69969be
kalimir <koloman.kovalik@studentagency.cz>
2020-02-20 16:03:00 +0100

    css changes + footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header.js'
+import Footer from './Components/Footer.js';
 
 function App() {
   return (
     <div className="App">
       <Header telNumber="+420 720 123 456"></Header>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css"
+
+export default function Footer() {
+  return (
+    <div className="footerContainer">
+      <div className="footerHeader">Chcete nas navstivit?</div>
+      <div className="footerBody">
+            <div className="footerElement">Tu bude obrazok</div>
+            <div className="footerElement">Tu bude adresa</div>
+            <div className="footerElement">osledna kategoria: Kontaktujte nas FB?</div>
+      </div>
+    </div>
+  );
+}
