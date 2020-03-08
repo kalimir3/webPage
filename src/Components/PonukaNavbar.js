e1ce473b6fdb6f7d9ca09abc79cfd5a497c86fff
unknown <kalimir3@gmail.com>
2020-03-08 11:35:21 +0100

    some bullshit

---
+++ src/Components/PonukaNavbar.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText
+} from 'reactstrap';
+
+const PonukaNavbar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+       
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/ponuka">Ponuka</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/ponuka/financovanie">Financovanie</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/ponuka/niecoDalsie">Financovanie</NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default PonukaNavbar;
---
+++ src/Pages/Financovanie.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Financovanie () {
+    return(
+        <div>
+            
+        </div>
+    )
+}
